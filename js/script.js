3f6b689ab38bb6ff3bdaef83965325db01ce76e6
ChronoAdmin <chrono.official@outlook.jp>
2023-03-07 14:35:59 +0900

    js完成　ページごとに設定するだけで完成

--- js/script.js
+++ js/script.js
@@ -11,58 +11,32 @@ toggle.addEventListener("click", () => {
 // アンダーライン start-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-$(window).scroll(function (){
-  $(".js-markerScrollAnimation").each(function(){
-    var position = $(this).offset().top; //ページの一番上から要素までの距離を取得
-    var scroll = $(window).scrollTop(); //スクロールの位置を取得
-    var windowHeight = $(window).height(); //ウインドウの高さを取得
-    if (scroll > position - windowHeight){ //スクロール位置が要素の位置を過ぎたとき
-      $(this).addClass('is-active'); //クラス「active」を与える
-    }
-  });
-});
+
 // アンダーライン end-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 //scroll_effect Start--------------------------------------------------------------------------------------------------------------------------------------------------------------
-$(window).scroll(function () {
-  var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right , .scroll_left_title , .underEffect');
-  var scrollAnimationFunc = function () {
-    for (var i = 0; i < scrollAnimationElm.length; i++) {
-      var triggerMargin = 150;
-      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-        scrollAnimationElm[i].classList.add('on');
-      }
-    }
+window.addEventListener('scroll', function(){
+  const headerH1 = document.querySelector(".header-title")
+  const headerNav = document.querySelector("#header-nav")
+  console.log(this.window.scrollY)
+  if (window.scrollY > 500) {
+    headerH1.classList.add("none")
   }
-  window.addEventListener('load', scrollAnimationFunc);
-  window.addEventListener('scroll', scrollAnimationFunc);
-});
-
-
-$(window).scroll(function () {
-  if($(window).scrollTop() > 800) {
-    $('#header-nav').addClass('none');
-    $('.header-logo').addClass('none');
-  } else {
-    $('#header-nav').removeClass('none');
-    $('.header-logo').removeClass('none');
+  else if (window.scrollY < 500) {
+    headerH1.classList.remove("none")
   }
-});
-$(window).scroll(function () {
-  if($(window).scrollTop() > 300) {
-    $('.header-title').addClass('none');
-  } else {
-    $('.header-title').removeClass('none');
+  if (window.scrollY > 900) {
+    headerNav.classList.add("none")
+  }
+  else if (window.scrollY < 900) {
+    headerNav.classList.remove("none")
   }
 });
 
 
-$(".mv video").on("loadedmetadata",function(){
-  $("body").addClass("close")
-  $(".loadingBody").fadeOut(1500)
-})
+
 //scroll_effect End--------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
